376dba3213920dd62e83db056d6f5c24ca382a4a
CUAN O CONCHUIR DILLON <G00300230@gmit.ie>
2020-02-16 14:34:50 +0000

    Basic player movement

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Movement
+    public float speed;
+    Vector2 movePosition;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePosition = new Vector2(Input.GetAxis("Horizontal")*speed*Time.deltaTime, Input.GetAxis("Vertical")*speed*Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+movePosition);
+    }
+}
