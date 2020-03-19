10d228cc8e5d4751b83265c77e1e126234120d1f
CUAN O CONCHUIR DILLON <G00300230@gmit.ie>
2020-03-19 16:18:10 +0000

    Added bullets

---
+++ Assets/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(transform.eulerAngles.y == 0)
+        {
+            rb.MovePosition(rb.position + Vector2.right * speed * Time.deltaTime);
+        }
+        if(transform.eulerAngles.y == 180f)
+        {
+            rb.MovePosition(rb.position + Vector2.left * speed * Time.deltaTime);
+        }
+        
+    }
+}
--- Assets/__Scripts/Player.cs
+++ Assets/__Scripts/Player.cs
@@ -6,7 +6,13 @@ public class Player : MonoBehaviour
 {
     //Movement
     public float speed;
+    public GameObject bullet;
+    public float timeBetweenSpawn;
+    public Transform bulletPosition;
+    bool called = false;
+
     Vector2 movePosition;
+
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -19,10 +25,48 @@ public class Player : MonoBehaviour
     void Update()
     {
         movePosition = new Vector2(Input.GetAxis("Horizontal")*speed*Time.deltaTime, Input.GetAxis("Vertical")*speed*Time.deltaTime);
+        movePosition = (rb.position + movePosition);
+        movePosition.x = Mathf.Clamp(movePosition.x, -12f, 12f);
+        movePosition.y = Mathf.Clamp(movePosition.y, -4.7f, 4.15f);
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180f, 0);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(!called)
+            {
+                called = true;
+                StartCoroutine(SpawnBullet());
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            called = false;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position+movePosition);
+        rb.MovePosition(movePosition);
+    }
+
+    IEnumerator SpawnBullet()
+    {
+        yield return new WaitForSeconds(timeBetweenSpawn);// waits for time to spawn
+        Instantiate(bullet, bulletPosition.position, bulletPosition.rotation);
+
+        if(called)
+        {
+            StartCoroutine(SpawnBullet());
+        }
+        
     }
 }
